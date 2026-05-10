fc47c2076eae87c735df3bbc79c13284b00730d1
Brooks Bryant <brooks.bryant@icloud.com>
2026-05-10 12:43:53 -0500

    better js

--- main.js
+++ main.js
@@ -1,17 +1,5 @@
-let theme = 'light';
 
 function toggleTheme() {
-  const elements = document.querySelectorAll('.body');
-
-  elements.forEach(element => {
-    if (theme === 'light') {
-      element.style.backgroundColor = '#2f2f2f';
-      element.style.color = '#f0f0f0';
-    } else {
-      element.style.backgroundColor = '#f0f0f0';
-      element.style.color = '#030303';
-    }
-  });
-
-  theme = theme === 'light' ? 'dark' : 'light';
+  document.body.classList.toggle('dark');  
+  document.body.classList.toggle('light');
 }
